08be05b9eba54c4960b9ab4a849959028e3fde45
fdt9603 <2763684408@qq.com>
2026-02-07 14:50:06 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,24 +8,3 @@ def func(a, b):
 func(a=1, b=2)
 print(func(a=1, b=2))
 
-def func(a, b):
-    print(a + b)
-    c=a+b
-    return none
-func(a=1, b=2)
-print(func(a=1, b=2))def func(a, b):
-    print(a + b)
-    c=a+b
-    return none
-func(a=1, b=2)
-print(func(a=1, b=2))def func(a, b):
-    print(a + b)
-    c=a+b
-    return none
-func(a=1, b=2)
-print(func(a=1, b=2))def func(a, b):
-    print(a + b)
-    c=a+b
-    return none
-func(a=1, b=2)
-print(func(a=1, b=2))
